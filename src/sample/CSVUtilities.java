eb8bbabdb12f2370d04936eb44214a452d5d6d6b
BT_1N3_31 <@>
2018-12-13 10:13:05 -0500

    @@

--- src/sample/CSVUtilities.java
+++ src/sample/CSVUtilities.java
@@ -1,21 +1,42 @@
 package sample;
 import  java.io.File;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CSVUtilities
 {
-    private final BufferedReader objReader;
     private ArrayList<String> CSVData;
     private int numColumns;
     private String[] container;
     public CSVUtilities(File csv)
     {
-        objReader = new BufferedReader(new FileReader("1.csv"));
+        try {
+           BufferedReader objReader = new BufferedReader(new FileReader("1.csv"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        this.numColumns = CSVData.size();
     }
-    public String getColumnHeaders()
+    public List<String> getColumnHeaders()
     {
-        for(int i =0; i< )
+        for(int i =0; i< numColumns; i++)
+        {
+
+        }
+    }
+    public List<String> getDataString(int column)
+    {
+
+    }
+    public List<Integer> getDataInt(int column)
+    {
+
+    }
+    public List<double> getDataDouble(int column)
+    {
+
     }
 }
